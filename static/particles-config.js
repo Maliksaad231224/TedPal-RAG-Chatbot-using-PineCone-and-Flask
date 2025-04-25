f8adebaaf985ad76c690afb46decaedba40b4ea8
saad <maliksaadahmed17@gmail.com>
2025-04-25 19:45:36 +0500

    working on frontend

--- static/particles-config.js
+++ static/particles-config.js
@@ -1,9 +1,9 @@
 particlesJS("particles-js", {
     particles: {
-        number: { value: 60, density: { enable: true, value_area: 800 } },
+        number: { value: 120, density: { enable: true, value_area: 900 } },
         color: { value: ["#ff0080", "#00ffff", "#ffcc00"] },
         shape: { type: "circle" },
-        opacity: { value: 0.7, random: true },
+        opacity: { value: 0.2, random: true },
         size: { value: 8, random: true },
         move: { enable: true, speed: 3, direction: "none", random: false }
     },
